c4417f2c6858572bde49ffcbade943d6c2d8d171
Anthony Decker <38474460+Ajd100@users.noreply.github.com>
2018-04-18 09:32:19 -0600

    Uploading directory of all project files
    bitonicSort.java is basic, uses arrays to sort any array given. - Doesn't work unless it's a power of two and can't pad because it's an array
    bitonicSort2.java is identical to bitonicSort.java but showcases what happens when you try to sort an array that isn't a power of 2
    finalBitonicSort.java uses an ArrayList<Integer> to sort - Allows padding to power of 2 to sort successfully. Then cleans up output and deletes all 0's

---
+++ Final Project/bitonicSort.java
@@ -0,0 +1,117 @@
+// Bitonic Merge Sort
+// Anthony Decker
+// CSIS_3000 Final Project
+
+import java.math.*;
+
+public class bitonicSort {
+	public static void main(String args[]) {
+		//Creating time variables to show the computation time of sorting with this algorithm
+		long time, startTime, endTime = (long) 0.0;
+		int ascend = 1;
+		int descend = 0;
+		//Initializing the Array with an amount power of 2
+		//int a[] = new int[(int)Math.pow(2,4)];
+		int a[] = {5, 18, 7, 2, 9, 3, 19, 1};
+		System.out.println("Unsorted array: ");
+		//Printing out the randomly generated numbers in the array
+			for (int i = 0; i < a.length; ++i){
+		//		a[i] = (int) ((Math.random()*1000) + 1);
+				System.out.print(a[i] + " ");
+			}
+		//Creating bitonicSort object to run the outside methods
+		bitonicSort bs = new bitonicSort();
+		
+//		---------------------------------------------------------------------------------------------------------------------------------
+		/*Sorting in ascending order*/
+		
+		startTime = System.currentTimeMillis();
+		bs.sort(a, a.length, ascend);
+		endTime = System.currentTimeMillis();
+		time = (endTime - startTime);
+		
+		System.out.println("\n\nAscending Sorted array:");
+		//Printing the sorted Array Ascending
+		for (int i = 0; i < a.length; ++i){
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("\nThe time of completion for this sort is: " + time + "ms");
+		
+//		---------------------------------------------------------------------------------------------------------------------------------
+		/*Sorting in descending order*/
+		
+		startTime = System.currentTimeMillis();
+		bs.sort(a, a.length, descend);
+		endTime = System.currentTimeMillis();
+		time = (endTime - startTime);
+		
+		System.out.println("\nDescending Sorted array:");
+		//Printing the sorted Array Descending
+		for (int i = 0; i < a.length; ++i){
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("\nThe time of completion for this sort is: " + time + "ms");
+	}
+	
+	//I have this method so that I don't have to statically call the information.
+	//Takes the array, it's length, and what direction it will be sorted in and passes it on to another method.
+	
+	public void sort(int a[], int arrLength, int dir) {
+		bitonicSort(a, 0, arrLength, dir);
+	}
+	
+	//This is the method that makes it bitonicMergeSort RECURSIVE
+	//It splits the array in half and sorts the first half in ascending order 
+	//The second half is sorted in descending order
+	//Once they both have been sorted, they are merged into the direction specified
+	
+	public void bitonicSort(int a[], int low, int cnt, int dir) {
+		if (cnt > 1) {
+			int k = cnt/2;
+
+			// sort in ascending order
+			bitonicSort(a, low, k, 1);
+			/* for(int i = 0; i < a.length; ++i){
+				System.out.print(a[i] + " ");
+			} 
+			System.out.print("\n"); */
+			// sort in descending order
+			bitonicSort(a,low+k, k, 0);
+		
+			// Will merge whole sequence in whichever sequence dir is given
+			bitonicMerge(a, low, cnt, dir);
+			
+		}
+	}
+	
+	//This is a recursive method to "merge" the parallel arrays
+	//Both the bitonicMerge method calls are given the array, the starting position
+	//how big the 'partitioned' array is, and the direction of sorting
+	
+	public void bitonicMerge(int a[], int low, int cnt, int dir) {
+		if (cnt > 1) {
+			int k = cnt/2; //This creates an integer to split the array in 2
+			for (int i = low; i < low + k; ++i) {
+				swapVar(a,i, i+k, dir);
+			}
+			//This is for printing out the entire sort to the console.
+		/* 	for(int i = 0; i < a.length; ++i){
+				System.out.print(a[i] + " ");
+			} 
+			System.out.print("\n"); */
+			bitonicMerge(a, low, k, dir);
+			bitonicMerge(a, low+k, k, dir);
+		}
+	}
+	
+	//Checking which way it needs to be sorted and if the elements match the parameters, swap them
+	
+	public void swapVar(int a[], int i, int j, int dir) {
+		if ((a[i] > a[j] && dir == 1) || (a[i] < a[j] && dir == 0)) {
+			// Swapping elements
+			int temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
+	}	
+}
